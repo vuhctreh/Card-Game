20566ff63faed0c354b0030158e5126db05fbe89
Victor <victor.lecardonnel@hotmail.fr>
2020-11-10 12:21:27 +0100

    new initial commit

---
+++ coursework/src/main/java/uk/coolguys/main/Main.java
@@ -0,0 +1,36 @@
+package uk.coolguys.main;
+
+import uk.coolguys.Table;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import static uk.coolguys.utils.PrintUtils.println;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        final Scanner scanner = new Scanner(System.in);
+
+        // Asks for the amount of players
+        println("Please enter the number of players: ");
+        int nPlayers = scanner.nextInt();
+
+        // Asks for the card deck
+        println("Please enter the path to the pick (card deck):");
+        File pickFile = new File(scanner.next());
+        if (!pickFile.exists() || !pickFile.canRead())
+            throw new FileNotFoundException("The path to the pick seems incorrect!");
+
+        List<Integer> cards = Files.readAllLines(Paths.get(pickFile.toURI())).stream().map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        // Récupérer cartes & joueurs
+        Table.begin(cards, nPlayers).distribute();
+    }
+
+}
